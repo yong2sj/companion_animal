2545ab6c71e76dd0d14a43bf6579ad80026602f5
yongheelee <ekor11@daum.net>
2022-08-02 16:24:59 +0900

    로그인 페이지 이동 수정

--- companion_animals/common/views.py
+++ companion_animals/common/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
 from .forms import UserForm
 
+
 def signup(request):
     """
     회원가입
@@ -14,20 +15,20 @@ def signup(request):
             form.save()
             # 로그인 페이지를 보여줄 때
             # return redirect('common:login')
-            
+
             # form.cleaned_data.get() : 넘어온 폼에서 화면의 입력 값을 얻기 위한 함수
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
 
             # authenticate() : 사용자 아이디와 비밀번호를 담아서 자격증명을 요청하는 함수
-            user = authenticate(username=username, password= raw_password)
+            user = authenticate(username=username, password=raw_password)
 
             if user is not None:
                 # login() : 로그인 해주는 함수
                 login(request, user)
-            return redirect('index')
+            return redirect("board:index")
 
     else:
         form = UserForm()
 
-    return render(request, 'common/signup.html', {'form':form})
+    return render(request, "common/signup.html", {"form": form})
