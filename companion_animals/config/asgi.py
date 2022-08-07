905757cf70f102a12d0c76651a0e711f91947a0f
yeomdonggeun <meronot@naver.com>
2022-08-07 17:35:11 +0900

    asgi.py 코드정리

--- companion_animals/config/asgi.py
+++ companion_animals/config/asgi.py
@@ -4,18 +4,15 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-
 import os
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "movieTalk.settings")
-# django.setup()
-django_asgi_app = get_asgi_application()
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "movieTalk.settings")
+# django.setup()
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
